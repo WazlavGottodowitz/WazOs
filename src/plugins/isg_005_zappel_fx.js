a516a23972df78dbeace1b4bbbf121fcc9173567
WazlavGottodowitz <klauskl03te@gmail.com>
2026-05-23 16:11:42 +0200

    Update isg_005_zappel_fx.js

--- src/plugins/isg_005_zappel_fx.js
+++ src/plugins/isg_005_zappel_fx.js
@@ -1,21 +1,6 @@
 window.isg_005_zappel_fx = {
-  intensity: 20, // Die "Zappel-Stärke"
-
-  init: function(manager) { this.manager = manager; },
-
-  process: function(actionType, signal, state) {
-    if (actionType === 'DRAG_MOVE') {
-      // Der Zappel-Algorithmus: 
-      // Erzeugt ein unkontrolliertes Zittern, das mit der Zeit oder 
-      // basierend auf der Mausgeschwindigkeit zunimmt.
-      const jitterX = (Math.random() - 0.5) * this.intensity;
-      const jitterY = (Math.random() - 0.5) * this.intensity;
-
-      return {
-        x: signal.x + jitterX,
-        y: signal.y + jitterY
-      };
-    }
-    return signal;
+  process: function(type, signal, state) {
+    // "VST-Transformation" des Signals
+    return { x: signal.x + Math.random(), y: signal.y + Math.random() };
   }
 };
