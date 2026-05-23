c9c398cb5928a02cdb800e5a61e5022328cb6b3e
WazlavGottodowitz <klauskl03te@gmail.com>
2026-05-23 17:43:45 +0200

    Update isg_005_zappel_fx.js

--- src/plugins/isg_005_zappel_fx.js
+++ src/plugins/isg_005_zappel_fx.js
@@ -1,12 +1,8 @@
 window.isg_005_zappel_fx = {
   intensity: 30,
-
   init: function(manager) { this.manager = manager; },
-
   process: function(actionType, signal, state) {
-    // ABSTURZ-SCHUTZ: Wenn kein Signal da ist, gib das, was da ist, einfach zurück
-    if (!signal) return signal; 
-
+    if (!signal || typeof signal.x !== 'number') return signal;
     if (actionType === 'DRAG_MOVE' && state.draggingId) {
       return {
         x: signal.x + (Math.random() - 0.5) * this.intensity,
