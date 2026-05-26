c8980de39333453a01ac65f4f76971e8cb3d8e77
WazlavGottodowitz <klauskl03te@gmail.com>
2026-05-26 20:09:37 +0200

    Create WazgTheme.js

---
+++ src/ui/WazgTheme.js
@@ -0,0 +1,14 @@
+const modules = [
+  'src/core/WazgLogcat.js',
+  'src/core/WazgManager.js',
+  'src/core/WazgGuard.js',
+  'src/core/WazgAnatomy.js',
+  'src/ui/WazgUI.js',
+  'src/ui/WazgTheme.js',           // ← Jetzt vorhanden
+  'src/ui/WazgAccessibility.js',
+  'src/ui/WazgPanZoom.js',
+  'src/ui/WazgFrameThrower.js',
+  'src/ui/WazgPathMorph.js',
+  'src/ui/WazgBezierEditor.js',
+  'src/ui/WazgLayout.js'
+];
