309d6e15f445d36887a11fd94c46a5c5c6a6c518
WazlavGottodowitz <klauskl03te@gmail.com>
2026-05-27 05:14:07 +0200

    Delete src directory

--- src/plugins/isg_001_guard.js
+++
@@ -1,6 +0,0 @@
-window.isg_001_guard = {
-  process: function(type, signal, state) {
-    // Return false um zu stoppen, oder signal für durchlass
-    return true; 
-  }
-};
--- src/plugins/isg_002_rig_processor.js
+++
@@ -1,9 +0,0 @@
-window.isg_002_rig_processor = {
-  init: function(manager) { this.manager = manager; },
-
-  process: function(actionType, signal, state) {
-    // Hier könnte man später IK-Berechnungen einbauen
-    // Aktuell: Signal durchreichen
-    return signal;
-  }
-};
